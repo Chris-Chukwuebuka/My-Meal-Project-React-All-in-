ad1bd5c7348dca274d3e3b9a610e3170f3e0787d
Ebuka-Chucks <Chrisbrandford4@gmail.com>
2023-08-09 14:17:34 +0100

    updated

--- src/Components/Banners.jsx
+++ src/Components/Banners.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className="banner container d-md-flex py-4 justify-content-md-start   align-items-center">
       
           <div className="text-start  mx-2">
-        <p className="display-5 text-primary fw-bold">Healthy Foods </p>
+        <p className="display-5 text-white fw-bold">Healthy Foods </p>
         <h1 className="text-dark fw-bolder">Explore Different Meals</h1>
         <button className="btn btn-primary text-white my-2">See More Below</button>
       </div>
